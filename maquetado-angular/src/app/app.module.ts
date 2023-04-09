316da636385cb2762202cd31e4bd0e53dd0361b1
Georqx <sputnikuno@outlook.com>
2023-04-09 17:39:43 -0300

    maquetado angular

---
+++ maquetado-angular/src/app/app.module.ts
@@ -0,0 +1,39 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { EncabezadoComponent } from './componentes/encabezado/encabezado.component';
+import { BannerComponent } from './componentes/banner/banner.component';
+import { AcercadeComponent } from './componentes/acercade/acercade.component';
+import { ExperienciasComponent } from './componentes/experiencias/experiencias.component';
+import { EducacionComponent } from './componentes/educacion/educacion.component';
+import { SkillsComponent } from './componentes/skills/skills.component';
+import { ProyectosComponent } from './componentes/proyectos/proyectos.component';
+import { FooterComponent } from './componentes/footer/footer.component';
+import { NgCircleProgressModule } from 'ng-circle-progress';
+import { LoginComponent } from './componentes/login/login.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    EncabezadoComponent,
+    BannerComponent,
+    AcercadeComponent,
+    ExperienciasComponent,
+    EducacionComponent,
+    SkillsComponent,
+    ProyectosComponent,
+    FooterComponent,
+    LoginComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgCircleProgressModule.forRoot({})
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
