ba437dd50c1a1fd3e50d2a5e9dde45570e6584ce
Georqx <sputnikuno@outlook.com>
2023-06-25 14:36:16 -0300

    actualizacion

--- maquetado-angular/src/app/app-routing.module.ts
+++ maquetado-angular/src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { HomeComponent } from './componentes/home/home.component';
 import { LoginComponent } from './componentes/login/login.component';
 import { NewExperienciaComponent } from './componentes/experiencias/new-experiencia.component';
 import { EditExperiencaComponent } from './componentes/experiencias/edit-experienca/edit-experienca.component';
+import { AcercadeComponent } from './componentes/acercade/acercade.component';
 
 const routes: Routes = [
   {path:'', component: HomeComponent},
   {path:'login', component: LoginComponent},
   {path: 'nuevaexp', component: NewExperienciaComponent},
-  {path: 'editexp/:id', component: EditExperiencaComponent}
+  {path: 'editexp/:id', component: EditExperiencaComponent},
+
 
 ];
 
